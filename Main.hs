43a0740edb6366f44cd4bf9dea7137c822f1b47a
Bladymir Tellez <btellez@gmail.com>
2017-02-04 20:57:32 -0800

    Implement Simple Quiz Functionality

--- Main.hs
+++ Main.hs
@@ -5,7 +5,33 @@ import Data.Text
 
 main :: IO ()
 main = do
-  pairs <- splitEntries . List.map pack  . List.lines <$> readFile "lang/de-en.txt"
-  mapM_ print pairs 
+  pairs <- splitEntries . packLines <$> readFile "lang/de-en.txt"
+  mapM_ quiz pairs
 
+
+quiz :: [Text] -> IO ()
+quiz (foreignTerm:englishTerm:_) =
+  askEnToForeign englishTerm >> pack <$> getLine >>= confirmAnswer foreignTerm
+
+
+confirmAnswer :: Text -> Text -> IO ()
+confirmAnswer foreignTerm userAnswer
+  | anwerIsCorrect == True = putStrLn "\tCorrect!"
+  | anwerIsCorrect == False = putStrLn $ "\tWrong!  The answer is: " ++ (unpack foreignTerm)
+  where anwerIsCorrect = userAnswer == foreignTerm
+
+
+askEnToForeign :: Text -> IO ()
+askEnToForeign term = putStrLn $ "\nHow do you say: \"" ++ (unpack term) ++ "\"?"
+
+
+printSecond :: [Text] -> IO ()
+printSecond (x1:x2:_) = putStrLn . unpack $ x2
+
+
+packLines :: String -> [Text]
+packLines = List.map pack . List.lines
+
+
+splitEntries :: [Text] -> [[Text]]
 splitEntries = List.map (splitOn (pack "|"))
